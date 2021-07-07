6333fa8fc1daec103a0d96db6812a2523dd76797
adeline-huvet <adeline.huvet.emploi@gmail.com>
2021-07-07 17:51:07 +0200

    1ère mise en place du projet ajout de react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Header} from './Components/Header';
+import { Watchlist } from './Components/Watchlist';
+import {Watched} from './Components/Watched';
+import { Add } from './Components/Add';
+
 import './App.css';
+
+
 
 function App() {
   return (
-    <h1>Hello world</h1>
+       <Router>
+          <Header/>
+          <Switch>
+              <Route exact path="/">
+                  <Watchlist/>
+              </Route>
+
+            <Route path="/watched">
+              <Watched/>
+            </Route>
+
+            <Route path="/add">
+              <Add/>
+            </Route>
+          </Switch>
+      </Router>
   );
 }
 
